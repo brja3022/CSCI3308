14c98d04af182a82167a1a29bf296d9d65dcd7bc
CodisMc <comc4648@colorado.edu>
2019-07-02 16:41:49 -0600

    created our scripts.sql but we need to rework the actual script and functions

---
+++ Database/scripts.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS c_functions(
+function_id INT NOT NULL,
+name VARCHAR(50) NOT NULL
+Note: Make this once 
+);
+
+CREATE TABLE IF function is *blank*(
+//table specific id
+//incrementor
+//function_id (to link tables)
+//return condition
+//methods inside
+//condition_id
+
+NOTE: Not all of these tables will be the same. They will require specific 
+information based on what each of the statements do. We have to do this for all
+statements (should be 12 tables).
+);
+
+CREATE TABLE conditions(
+//var1
+//operators
+//var2
+NOTE: These are specific to each kind of user session.
+);
